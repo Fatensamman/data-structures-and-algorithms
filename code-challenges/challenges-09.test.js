3a67a426d62e391061bbe2929d5f155df589f587
Fatensamman <arch.fatensamman@gmail.com>
2021-02-27 13:47:12 +0200

    .

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -19,7 +19,12 @@ Becomes:
 ------------------------------------------------------------------------------------------------ */
 
 function transformToLis(obj) {
-
+let key = Object.keys(obj);
+let value=Object.values(obj);
+let newarray=keys.map((item,index) =>{
+return `<li>${item} : ${value[index]}</li>`;
+})
+return newarray;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -306,7 +311,7 @@ DO NOT CHANGE any of the below code.
 Run your tests from the console: jest challenges-09.test.js
 ------------------------------------------------------------------------------------------------ */
 
-xdescribe('Testing challenge 1', () => {
+describe('Testing challenge 1', () => {
   test('It should return a list of key value pairs inside of li tags', () => {
     expect(transformToLis({ name: 'bob', age: 32 })[0]).toStrictEqual(`<li>name: bob</li>`);
     expect(transformToLis({ name: 'bob', age: 32 })[1]).toStrictEqual(`<li>age: 32</li>`);
