648f3e0ad297af9f7aada95ee6b544f682e05e45
fatensamman <arch.fatensamman@gmail.com>
2021-04-18 17:50:55 +0300

    findmax

--- javascript/code-challenges/tree/__tests__/tree.test.js
+++ javascript/code-challenges/tree/__tests__/tree.test.js
@@ -57,4 +57,30 @@ describe('Binary tree', () => {
         const postOrder = tree.postOrder();
         expect(postOrder).toEqual(expected);
     });
+    it('postOrder', () => {
+        const one = new Node(1);
+        const two = new Node(2);
+        const three = new Node(3);
+        const four = new Node(4);
+        const five = new Node(5);
+        const six = new Node(6);
+        const seven = new Node(7);
+        const eight = new Node(8);
+        one.left = two;
+        one.right = three;
+        two.left = four;
+        two.right = five;
+        five.left = eight;
+        three.left = six;
+        three.right = seven;
+        let tree = new BinaryTree(one);
+        const expected = 8;
+        const findMax = tree.findMax();
+        expect(findMax).toEqual(expected);
+    });
+    it('sholud return expetion when the tree is empty',()=>{
+        let newTree = new BinaryTree();
+        let findMax = newTree.findMax()
+        expect(findMax).toEqual('exception');
+    })
 });
